4060b43891bb01e1e28aaeface0b23916d63590f
Gibbo Marra <marragabriel@itis-molinari.eu>
2025-01-28 09:46:02 +0100

    caricamento e rimozione

---
+++ public/index.js
@@ -0,0 +1,110 @@
+const ul = document.getElementById("ul");
+const button = document.getElementById("submit");
+const input = document.getElementById("inputText");
+let list = [];
+let count = 0;
+const myToken = "67fea5bf-6439-4377-bf9b-65f43a7459e9";
+const myKey = "chiave";
+
+
+const caricaLista=()=> {
+  fetch("/todo", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.todos);
+    list = data.todos;
+    render();
+  })
+}
+caricaLista();
+
+button.onclick = () => {
+  const data = {
+    inputValue: input.value,
+    completed: false,
+  };
+
+  // Invia il nuovo elemento al server
+  fetch("/todo/add", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      list.push(result.todo); // Aggiorna la lista con l'elemento dal server
+      render();
+      input.value = ""; // Resetta l'input
+    });
+};
+
+function render() {
+  let html = "";
+  list.forEach((element, id) => {
+    let completedClass = element.completed ? "done" : "";
+    html += `<li id='li_${element.id}' class='divs ${completedClass}'>
+      ${element.inputValue}
+      <button type='button' class='pulsantiConferma' id='bottoneC_${element.id}'>conferma</button>
+      <button type='button' class='pulsantiElimina' id='bottoneE_${element.id}'>elimina</button>
+    </li>`;
+  });
+  ul.innerHTML = html;
+
+  document.querySelectorAll(".pulsantiElimina").forEach((button) => {
+    button.onclick = () => {
+      const id = button.id.replace("bottoneE_", "");
+      deleteTODO(id);
+    };
+  });
+
+  document.querySelectorAll(".pulsantiConferma").forEach((button) => {
+    button.onclick = () => {
+      const id = button.id.replace("bottoneC_", "");
+      completeTodo(id);
+    };
+  });
+}
+
+
+const completeTodo = (id) => { 
+  const todo = list.find((item) => item.id === id);
+  fetch("/todo/complete", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  })
+    .then((response) => response.json())
+    .then(() => {
+      if(!todo.completed){
+      todo.completed = true;
+      }else{
+        todo.completed=false
+      } // Aggiorna lo stato localmente
+      render();
+    });
+}
+
+const deleteTODO=(id)=> { //remove
+  fetch(`/todo/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then(() => {
+      list = list.filter((item) => item.id !== id);
+      render();
+    });
+}
+
+render();
